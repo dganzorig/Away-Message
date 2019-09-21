8a6dceaa9ccf993aad38c4a1fabe9a26d41edb21
Dona Ganzorig <ganzorig0518@uchicago.edu>
2019-09-21 18:33:03 -0500

    Edited tweet functionality to make use of persistent session store

--- Away Message/cell/CategorySuggestionCell.swift
+++ Away Message/cell/CategorySuggestionCell.swift
@@ -26,31 +26,43 @@ class CategorySuggestionCell: UITableViewCell {
     }
     
     @IBAction func twitterPressed(_ sender: UIButton) {
-        TWTRTwitter.sharedInstance().logIn { (session, error) in
-            if (session != nil) {
-                // Log-in successful, so compose the Tweet
-                let composer = TWTRComposer()
-                
-                guard let message: String = self.messageLabel.text else { return }
-                let starterMessage: String = "Check out this away message I use for \(AppConstants.twitterHandle):\n\n\(message)"
-                composer.setText(starterMessage)
-                
-                guard let vc: CategorySuggestionViewController = self.viewController else { return }
-                
-                composer.show(from: vc, completion: { (result) in
-                    if (result == .done) {
-                        // Successfully composed tweet
-                        vc.showToast(message: "Successfully posted tweet")
-                    } else {
-                        // Canceled composing
-                        vc.showToast(message: "Canceled Twitter posting")
-                    }
-                })
-            } else {
-                // Error logging in
-                self.viewController?.showToast(message: "There was an error logging into your Twitter account")
+        let store = TWTRTwitter.sharedInstance().sessionStore
+        
+        if store.session() == nil {
+            TWTRTwitter.sharedInstance().logIn { (session, error) in
+                if (session != nil) {
+                    // Log-in successful, so compose the Tweet
+                    self.composeTweet()
+                } else {
+                    // Error logging in
+                    self.viewController?.showToast(message: "There was an error logging into your Twitter account")
+                }
             }
+        } else {
+            // Already logged-in, so compose the Tweet
+            self.composeTweet()
         }
+    }
+    
+    // Assumption: TWTRTwitter has a valid session
+    private func composeTweet() {
+        let composer = TWTRComposer()
+        
+        guard let message: String = self.messageLabel.text else { fatalError() }
+        let starterMessage: String = "Check out this away message I use for \(AppConstants.twitterHandle):\n\n\(message)"
+        composer.setText(starterMessage)
+        
+        guard let vc: CategorySuggestionViewController = self.viewController else { fatalError() }
+        
+        composer.show(from: vc, completion: { (result) in
+            if (result == .done) {
+                // Successfully composed tweet
+                vc.showToast(message: "Successfully posted tweet")
+            } else {
+                // Canceled composing
+                vc.showToast(message: "Canceled Twitter posting")
+            }
+        })
     }
     
 }
